c6633d69ebdd1c7e897ff2bd31707c44572a02a3
Jabar <ovenbakedgoods88@gmail.com>
2020-04-16 22:43:36 -0500

    changing view

--- make_tacos/coldbeans/models.py
+++ make_tacos/coldbeans/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Coldbean(models.Model):
-    time_now = models.TextField()
+    channel_up= models.TextField()
     title = models.CharField(max_length=255)
-    times = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return (self.title)
