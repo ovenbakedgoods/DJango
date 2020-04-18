3c12a4e531186f6b4c8f89de4534b8010a079a48
Jabar <ovenbakedgoods88@gmail.com>
2020-04-17 23:18:33 -0500

    making a view inside app

--- make_tacos/coldbeans/views.py
+++ make_tacos/coldbeans/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Coldbean
+
+def Bring_Noise(request):
+    beans = Coldbean.objects.all()
+    output = " ".join(str(bean) for bean in beans)
+    return HttpResponse(output)
 
 # Create your views here.
--- make_tacos/make_tacos/urls.py
+++ make_tacos/make_tacos/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
+from django.conf.urls import include, url
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$',views.hello)
+    url(r'^$',views.hello),
+    url(r'^coldbeans/', include('coldbeans.urls')),
 ]
