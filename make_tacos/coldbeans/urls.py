548f5646843d055f6c335d19c95967247332316f
Jabar <ovenbakedgoods88@gmail.com>
2020-04-18 21:49:51 -0500

    Adjusting view for main page

--- make_tacos/coldbeans/urls.py
+++ make_tacos/coldbeans/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.Bring_Noise),
+    url(r'^$',views.bean_list),
 ]
--- make_tacos/coldbeans/views.py
+++ make_tacos/coldbeans/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Coldbean
 
-def Bring_Noise(request):
+def bean_list(request):
     beans = Coldbean.objects.all()
-    output = " ".join(str(bean) for bean in beans)
-    return HttpResponse(output)
+    return render(request,'coldbeans/bean_list.html',{'beans': beans})
 
 # Create your views here.
