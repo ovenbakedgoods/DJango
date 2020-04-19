29003af65ed9e76bee70785be3d47548970a6aa5
Jabar <ovenbakedgoods88@gmail.com>
2020-04-19 03:17:38 -0500

    Updating Stylesheet

--- make_tacos/make_tacos/urls.py
+++ make_tacos/make_tacos/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
 from django.conf.urls import include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
