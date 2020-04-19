6b6c04948c0d8e99f7975133584cbd47ff09570b
Jabar <ovenbakedgoods88@gmail.com>
2020-04-18 22:03:52 -0500

    updating templates in main project

--- make_tacos/make_tacos/views.py
+++ make_tacos/make_tacos/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def hello(request):
-    return HttpResponse("Ollie Ollie Oxen Free")
+    return render(request,'home.html')
